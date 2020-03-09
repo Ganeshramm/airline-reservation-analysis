5f1e859710db4cb1e7609bd8dc9fdc81b50ed4da
Ganesh Ram M <ganeshti@ymail.com>
2020-03-10 00:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,18 +19,20 @@ def home():
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
-
-    form_values = [ x for x in request.form.values()]
-                      
-    for j in range(0,len(form_values)):
-        form_values[j] = int(form_values[j])
-    pred = model.predict([np.array(form_values)])
     
-    if pred == 1:
-        return render_template('index.html', prediction_text = 'Potential Customer')
-    else:
-        return render_template('index.html', prediction_text = 'Not a Potential Customer')
-
+    try:
+        form_values = [ x for x in request.form.values()]
+        for j in range(0,len(form_values)):
+            form_values[j] = int(form_values[j])
+        pred = model.predict([np.array(form_values)])
+    
+        if pred == 1:
+            return render_template('index.html', prediction_text = 'Potential Customer')
+        else:
+            return render_template('index.html', prediction_text = 'Not a Potential Customer')
+    except:
+        return render_template('index.html', prediction_text = 'Please fill all the fields')
+    
 
 if __name__ == "__main__":
     app.run(debug = True)
