9489c147d2f12f8609e85e75b363c59d5d1116a1
Ganesh Ram M <ganeshti@ymail.com>
2020-03-09 22:26:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,29 +21,9 @@ def home():
 def predict():
 
     form_values = [ x for x in request.form.values()]
-    
-    print()
-    print()
-    print(form_values)
-    
-    mapper = {'Direct' :  0,'Network' : 1,'Organic' : 2,
-     'Ahmedabad' : 0,'Bangalore' : 1,'Cochin' : 2,'Chandigarh' : 3,'Chennai' : 4,'Delhi' : 5,
-     'Hyderabad' : 6,'Kolkata' : 7,'Mumbai' : 8,'New Delhi' : 9,'Surat' : 10,
-     'Female' : 0,'Male' : 1, 'None' : 0,'Base' : 1,'Silver' : 2,'Gold' : 3,'Platinum' : 4,
-    'Yes' : 1, 'No' : 0}
-    
-    cat = [1,2,4,5,7,8]
-    
-    for i in cat:
-        form_values[i] = mapper[form_values[i]]
                       
     for j in range(0,len(form_values)):
         form_values[j] = int(form_values[j])
-     
-    print()
-    print()
-    print(form_values)    
-    
     pred = model.predict([np.array(form_values)])
     
     if pred == 1:
